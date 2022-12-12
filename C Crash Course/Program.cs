e322c53da3f3113b7a264d4cc10c47d7e4378f47
Shivom <shivompaudel@icloud.com>
2022-12-12 13:16:56 -0600

    first commit

---
+++ C Crash Course/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+//Namespace
+namespace NumberGuesser
+{
+    //Main Class
+    class Program
+    {
+        //Entry Point Method
+        static void Main(string[] args)
+        {
+            //Start Here
+            Console.WriteLine("Hello World");
+        }
+    }
+}
